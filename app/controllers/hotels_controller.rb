bdf86ad242f9bff201316236df38eb8666e5e7df
satofumiya <sato.workfree@gmail.com>
2022-11-27 21:01:02 +0900

    ルーム登録機能と一覧機能の作成

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,12 +1,23 @@
 class HotelsController < ApplicationController
     before_action :authenticate_user!
     def index
+        @user = current_user
+        @hotels = @user.hotels
     end
 
     def new
+        @user = current_user
+        @hotel = Hotel.new
     end
 
     def create
+        @hotel = Hotel.new(params.require(:hotel).permit(:name, :hotel_introduction, :price, :address,:user_id))
+        if @hotel.save
+            flash[:notice] = "ルームの登録ができました。"
+            redirect_to root_path
+        else
+            render root_path
+        end
     end
 
     def show
