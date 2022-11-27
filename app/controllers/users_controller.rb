c6d2a69c8b58bb74cedd5793cd582a0675a80881
satofumiya <sato.workfree@gmail.com>
2022-11-27 20:08:31 +0900

    ルーティングの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,4 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to root_path
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:user_name, :user_icon, :user_introduction)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, :only => [:show]do
     member do
       get :profile
     end
   end
   resources :hotels
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+
 
 
 
