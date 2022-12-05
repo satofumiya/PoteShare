d34729253af955a1a2aec66ef571c4a16b9a81fd
satofumiya <sato.workfree@gmail.com>
2022-12-06 01:26:22 +0900

    フリーワード検索機能への変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,12 @@
 class SearchesController < ApplicationController
     def index
         @user = current_user
-        @hotels = Hotel.looks(params[:word], params[:column])
+        columnsList = params[:columns].split
+        columnsList.each do |column|
+            @hotels = Hotel.looks(params[:word], column)
+            if @hotels != nil?
+                break
+            end
+        end
     end
 end
