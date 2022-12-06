0c16d265f62061155b6ab3fdf062339f3076f68a
satofumiya <sato.workfree@gmail.com>
2022-12-06 23:10:43 +0900

    予約機能の日付関係のバリデーションの修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,13 +4,13 @@ class Reservation < ApplicationRecord
     validates :NumberOfGuests, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
 
     def date_before_start
-        if start < Date.today
+        if start.nil? || start < Date.today
         errors.add(:start, 'は今日以降のものを選択してください')
         end
     end
 
     def date_before_finish
-        if end_day < start
+        if end_day.nil? || end_day < start
         errors.add(:end_day, 'は開始日以降のものを選択してください')
         end
     end
